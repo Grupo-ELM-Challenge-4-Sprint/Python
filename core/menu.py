6dacaf2dc9378bbfdada96b9f4149e39cba43988
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-10-21 18:18:29 -0300

    Refatora menu do usuário para incluir opções de lembretes e farmácias, removendo tutoriais específicos e ajustando a função de login.

--- core/menu.py
+++ core/menu.py
@@ -1,6 +1,8 @@
 from core.guia_interativo_e_ia import *
-from core.crud import *
 from core.tutoriais import *
+from core.farmacias_proximas import *
+from core.lembretes import *
+from database.database import abre_conexao, fecha_conexao, cria_tabelas_se_nao_existirem
 from main import __name__
 
 
@@ -15,8 +17,7 @@ def mostrar_menu_principal():
     print("=" * 42)
     print("1. Cadastro")
     print("2. Login")
-    print("3. Recuperar Senha")
-    print("4. Ajuda e Informações Gerais")
+    print("3. Ajuda e Informações Gerais")
     print("0. Sair do Sistema")
     print("=" * 42)
 
@@ -33,10 +34,12 @@ modo_guia_interativo_ativo = False
 
 # Função para exibir o menu do usuário após login
 # Menu Usuário
-def menu_usuario_logado(usuario_logado):
+def menu_usuario_logado(usuario_logado, cursor, conexao):
     global modo_guia_interativo_ativo
     nome_usuario = usuario_logado["nome"]
     email_usuario = usuario_logado["email"]
+
+    # Conexão já estabelecida no main.py
 
     while True:
         print("\n╔══════════════════════════════════════════════╗")
@@ -44,81 +47,42 @@ def menu_usuario_logado(usuario_logado):
         if modo_guia_interativo_ativo:
             print("║       ⭐ Modo Guia Interativo ATIVO ⭐       ║")
         print("╚══════════════════════════════════════════════╝\n")
-        
-        print("1. Tutorial de como cadastrar no App HC")
-        print("2. Tutorial de como logar no App HC")
-        print("3. Tutorial Esqueci minha Senha no App HC")
-        print("4. Tutorial de como acessar Resultados de Exames")
-        print("5. Tutorial de como acessar Receitas Médicas")
-        print("6. Tutorial de como acessar Minhas Agendas (Consultas/Exames)")
-        print("7. Tutorial de como acessar as Teleconsulta")
-        print("8. Tutorial de como acessar Solicitação de Exames")
-        print("9. Tutorial de como acessar Solicitação de Documentos")
-        print("10. Tutorial de como acessar os Meus Dados")
-        print("11. IA e Guia Interativo")
-        if verificar_se_eh_admin(usuario_logado):
-            print("12. Acesso Administrativo")
+
+        print("1. Acessar Tutoriais")
+        print("2. Lembretes de Consultas")
+        print("3. Lembretes de Receitas")
+        print("4. Farmácias próximas")
+        print("5. IA e Guia Interativo")
         print("0. Sair (Logout)")
+
         print("=" * 46)
 
         opcao_login = input("Digite o número da opção desejada e pressione Enter: ")
 
-        # Cada bloco abaixo trata uma opção do menu do usuário
         match opcao_login:
-            case '1':  # Tutorial de Cadastro
-                tutorial_cadastro(modo_guia_interativo_ativo)
-                input("\nPressione Enter para voltar ao menu do usuário...")
-            
-            case '2':  # Tutorial de Login
-                tutorial_login(modo_guia_interativo_ativo)
+            case '1':  # Acessar Tutoriais
+                menu_tutoriais(modo_guia_interativo_ativo)
                 input("\nPressione Enter para voltar ao menu do usuário...")
 
-            case '3':  # Tutorial Esqueci Senha
-                tutorial_esqueci_senha(modo_guia_interativo_ativo)
+            case '2':  # Lembretes de Consultas
+                lembretes_consultas(modo_guia_interativo_ativo, usuario_logado["cpf"], cursor, conexao)
                 input("\nPressione Enter para voltar ao menu do usuário...")
 
-            case '4':  # Tutorial de Resultados
-                tutorial_resultados(modo_guia_interativo_ativo)
+            case '3':  # Lembretes de Receitas
+                lembretes_receitas(modo_guia_interativo_ativo, usuario_logado["cpf"], cursor, conexao)
                 input("\nPressione Enter para voltar ao menu do usuário...")
 
-            case '5':  # Tutorial de Receitas
-                tutorial_receitas(modo_guia_interativo_ativo)
+            case '4':  # Farmácias próximas
+                encontrar_farmacias_proximas()
                 input("\nPressione Enter para voltar ao menu do usuário...")
 
-            case '6':  # Tutorial de Agendas
-                tutorial_agendas(modo_guia_interativo_ativo)
-                input("\nPressione Enter para voltar ao menu do usuário...")
-
-            case '7':  # Tutorial de Teleconsulta
-                tutorial_teleconsulta(modo_guia_interativo_ativo)
-                input("\nPressione Enter para voltar ao menu do usuário...")
-
-            case '8':  # Solicitação de Exames (HC)
-                tutorial_solicitacao_exames(modo_guia_interativo_ativo)
-                input("\nPressione Enter para voltar ao menu do usuário...")
-
-            case '9':  # Solicitação de Documentos (HC)
-                tutorial_solicitacao_documentos(modo_guia_interativo_ativo)
-                input("\nPressione Enter para voltar ao menu do usuário...")
-
-            case '10':  # Meus Dados
-                tutorial_meus_dados(modo_guia_interativo_ativo)
-                input("\nPressione Enter para voltar ao menu do usuário...")
-
-            case '11':  # IA e Guia Interativo
+            case '5':  # IA e Guia Interativo
                 modo_guia_interativo_ativo = guia_interativo_e_ia(modo_guia_interativo_ativo)
-
-            case '12':  # Acesso Administrativo
-                if verificar_se_eh_admin(usuario_logado):
-                    menu_administrativo(usuario_logado)
-                else:
-                    print("\n❌ Acesso negado! Você não tem permissão de administrador.")
-                    input("Pressione Enter para continuar...")
 
             case '0':
                 print(f"\nSaindo do seu usuário... Até logo, {nome_usuario}!")
                 break
-            
+
             case _:
                 print("\nOpção inválida. Por favor, escolha uma opção válida do menu.")
                 if modo_guia_interativo_ativo:
